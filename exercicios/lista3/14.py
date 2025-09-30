e66fd25870eeb9ef879eca51da792e3b066d46b9
feyre <gbgalvao90@gmail.com>
2025-09-30 19:47:50 -0300

    rapadura

--- exercicios/lista3/14.py
+++ exercicios/lista3/14.py
@@ -1,2 +1,16 @@
 #Elabore um algoritmo que solicite ao usuário 10 números reais e ao final apresente o
 #maior e o menor deles.
+
+numero = []
+for c in range (10):
+    while True:
+        try:    
+            n1 = float(input(f"Digite o {c + 1} número: "))
+            numero.append(n1)
+        except ValueError:
+            print("Entrada não permitida. Por favor tente novamente ")
+        break
+
+numero.sort()
+for numeros in numero:
+    print(numeros)
